752f510ab11af82a46b818849a5ec701bde57d2c
iftekhar-amin524 <55458428+iftekhar-amin524@users.noreply.github.com>
2021-02-07 14:40:17 +0600

    lab1
    WT lab task 1

---
+++ Lab_Task1/registration.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> Registration Page </title>
+</head>
+
+<body>
+<h1>My Registration Page</h1>
+
+<form>
+<table>
+<tr> <td>First Name: </td>
+<td><input type="text" name="firstname"></td><tr>
+<br>
+<tr>
+<td>Email: </td>
+<td><input type="text" name="email"> </td></tr>
+<br>
+<tr>
+<td>User Name: </td>
+<td><input type="text" name="username"> </td></tr>
+<br>
+<tr>
+<td>Password: </td>
+<td><input type="text" name="password"> </td></tr>
+<br>
+<tr>
+<td>Confirm Password: </td>
+<td><input type="text" name="confirmpassword"> </td></tr>
+<br>
+
+
+<td>Gender</td>
+<td><input type="radio" name="gender" value="male">Male<br> </td></tr>
+<td><input type="radio" name="gender" value="female">Female<br> </td></tr>
+<td><input type="radio" name="gender" value="other">Other<br> </td></tr>
+
+<tr>
+<td>Date Of Birth: </td>
+<td><input type="date" name="dateofbirth"> </td></tr>
+<br>
+</table>
+<input type="submit" value="Submit">
+<input type="reset">
+
+</form>
+<img src="pic.jpg">
+<br>
+<a href="https://en.wikipedia.org/wiki/Peaky_Blinders_(TV_series)">Peaky Blinders</a>
+
+</body>
+
+</html>
